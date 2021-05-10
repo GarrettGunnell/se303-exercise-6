d64623460eb680435ef3c5738b2351c19d9ea882
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-05-09 23:21:54 -0700

    Replace draw_button parameter list with button object

--- main.rb
+++ main.rb
@@ -1,11 +1,5 @@
-def draw_button(label_text, x, y, foreground_color, is_dark_mode)
-    if is_dark_mode
-      # darken foreground color for dark mode
-      paint(label_text, x, y, foreground_color - 10, '#111111')
-    else
-      # lighten foreground color for non-dark mode
-      paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
-    end
+def draw_button(button)
+    paint(button.text, button.x, button.y, button.foreground_color, button.color)
 end
 
 class Button
