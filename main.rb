a824a17585e391a27e384a8fbfd300f3d76e3b92
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-05-09 23:37:52 -0700

    Remove dark mode parameter and replace with color parameter

--- main.rb
+++ main.rb
@@ -3,21 +3,13 @@ def draw_button(button)
 end
 
 class Button
-    attr_reader :text, :x, :y
+    attr_reader :text, :x, :y, :foreground_color, :color
 
-    def initialize(text, x, y, foreground_color, dark_mode=false)
+    def initialize(text, x, y, foreground_color, color)
         @text = text
         @x = x
         @y = y
         @foreground_color = foreground_color
-        @dark_mode = dark_mode
-    end
-
-    def foreground_color
-        @dark_mode ? @foreground_color - 10 : @foreground_color + 10
-    end
-
-    def color
-        @dark_mode ? '#111111' : '#E0E0E0'
+        @color = color
     end
 end
