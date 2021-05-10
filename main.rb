5df5f2ea1744b86b10e9c87a7c3c38942e2c96ff
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-05-09 23:16:48 -0700

    Stub button class

--- main.rb
+++ main.rb
@@ -6,4 +6,8 @@ def draw_button(label_text, x, y, foreground_color, is_dark_mode)
       # lighten foreground color for non-dark mode
       paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
     end
-  end
+end
+
+class Button
+
+end
